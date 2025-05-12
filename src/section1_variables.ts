f5a1d95b760e0b2f9f0380a70779ec9bc8137604
Mike <mclancy96@gmail.com>
2025-05-12 14:36:02 -0400

    Add variables lab

---
+++ src/section1_variables.ts
@@ -0,0 +1,17 @@
+// section1_variables.ts
+// ======================
+// TASK: Declare 3 variables with explicit types:
+// - a number called `age`
+// - a string called `firstName`
+// - a boolean called `isEnrolled`
+//
+// Then, assign values to each of them.
+//
+// BONUS: Try declaring a variable without an explicit type and see what type is inferred!
+
+// Your code here 👇
+
+
+
+// DO NOT EDIT BELOW (for test purposes only)
+export { }
