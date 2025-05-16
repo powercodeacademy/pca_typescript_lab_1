31760814a1cb8c886923f3d3969e15f038bbd480
Mike <mclancy96@gmail.com>
2025-05-16 14:30:57 -0400

    Add explicit type annotation function

--- test/section1_variables.test.ts
+++ test/section1_variables.test.ts
@@ -3,6 +3,7 @@ import * as ts from "typescript";
 import { readFileSync } from "fs";
 import { join } from "path";
 import vm from "vm";
+import { expectExplicitTypeAnnotation } from "../explicit_type_annotation";
 
 describe("Section 1 - Variables ", () => {
   let context: any = {};
@@ -23,40 +24,6 @@ describe("Section 1 - Variables ", () => {
     it(`should define a variable named '${name}' of type '${type}'`, () => {
       expect(context).to.have.property(name);
       expect(typeof context[name]).to.equal(type);
-    });
-  }
-
-  function expectExplicitTypeAnnotation(varName: string, typeName: string) {
-    it(`should declare '${varName}' variable with an explicit type annotation of '${typeName}'`, () => {
-      const filePath = join(__dirname, "../src/section1_variables.ts");
-      const tsCode = readFileSync(filePath, "utf8");
-      const sourceFile = ts.createSourceFile(
-        filePath,
-        tsCode,
-        ts.ScriptTarget.Latest,
-        true
-      );
-
-      let found = false;
-
-      function checkNode(node: ts.Node) {
-        if (
-          ts.isVariableDeclaration(node) &&
-          node.name.getText() === varName &&
-          node.type &&
-          node.type.getText() === typeName
-        ) {
-          found = true;
-        }
-        ts.forEachChild(node, checkNode);
-      }
-
-      checkNode(sourceFile);
-
-      expect(
-        found,
-        `${varName} variable must have an explicit type annotation of '${typeName}'`
-      ).to.be.true;
     });
   }
 
